67c160fb98c8ee2c7241905876bb2d162c055957
renan_macbookair <renan.escalante.chong@gmail.com>
2015-07-01 16:33:42 -0400

    commit before testing the area metric

--- plot_correlation_highlight_points.m
+++ plot_correlation_highlight_points.m
@@ -6,8 +6,17 @@ load('NaturalIsolates_GeneticDistance_CorrelationData')
 
 %% Highlight specific strains
 
-Highlighted_Strains={'YPS128','YPS163','YPS606'};
+%Highlighted_Strains={'YPS128','YPS163','YPS606'};
 %Highlighted_Strains={'YJM978','YJM975','YJM981'};
+%Highlighted_Strains={'BC187','YJM978'};
+
+%Highlighted_Strains={'YJM421','S288C'};
+
+%Strains with maximal genetic distance
+Highlighted_Strains={'IL-01','UWOPS03-461.4'};
+
+Highlighted_Strains={'YJM978','DBVPG1373'}
+
 
 [Highlight_x,Highlight_y]=highlight_in_correlation_plot(QueryStrains_matrix,QueryStrains_MeanGeneticDistance,Highlighted_Strains);
 
@@ -25,7 +34,16 @@ ylabel('Genetic distance')
 
 Set_fig_RE(hfig,14,14,14)
 
+%% Find strains with maximal genetic distance ('IL-01'    'UWOPS03-461.4')
+
+%find(QueryStrains_MeanGeneticDistance==max(QueryStrains_MeanGeneticDistance))
+%QueryStrains_matrix(536,:)
+
+%Find strain with maximal difference minimal genetic distance
+%find(abs(x-3.639)<0.001)
+
 %%
-%filename='Correlation_setpoint_genetic_distance_highlight_strains';
-%export_fig(filename, '-pdf','-transparent','-nocrop');
+Strain_filename=[Highlighted_Strains{:}];
+filename=['Correlation_setpoint_genetic_distance_highlight_strains' Strain_filename] ;
+export_fig(filename, '-pdf','-transparent','-nocrop');
 end
