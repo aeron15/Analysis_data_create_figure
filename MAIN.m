7c62d05682ec191591801d375d8da787dec771af
renan_macbookair <renan.escalante.chong@gmail.com>
2015-07-15 11:38:26 -0400

    Compute meiotic segregants parental strains

--- MAIN.m
+++ MAIN.m
@@ -8,12 +8,12 @@ if ~exist('../outputFigures')
 end
 
 %Analysis of data
-compute_setpoints_reference_BC187()
-
-%Comparison of data in figure 1 and 4
-compute_ANOVA_to_compare_groups
-
-driver_main_figures()
+% compute_setpoints_reference_BC187()
+% 
+% %Comparison of data in figure 1 and 4
+% compute_ANOVA_to_compare_groups
+% 
+% driver_main_figures()
 
 plot_meiotic_segregants()
 
